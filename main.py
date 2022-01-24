44671966f81c6f684d2f4288aee7b4aad23630ee
sduffy1 <98351766+sduffy1@users.noreply.github.com>
2022-01-24 19:45:32 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,39 @@
+let list: number[] = []
+let text_list: string[] = []
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(list + 1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(list - 0)
+})
+basic.forever(function () {
+    basic.showString("" + (list[0]))
+    text_list = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+    ]
+})
